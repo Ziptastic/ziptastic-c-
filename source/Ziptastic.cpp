a7bafe73b45cea4e5a89ba38b0d1d62123b02308
bfranco94 <bfranco@murraystate.edu>
2016-07-13 13:46:59 -0400

    Revert "Created Ziptastic.cpp file, and basic object shell"
    This reverts commit fc07c4ec4525f731778081ea2b451bce5f4104bc.

--- source/Ziptastic.cpp
+++ source/Ziptastic.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -12,38 +11,4 @@ class Ziptastic
         Ziptastic(string key); // Constructor
         ForwardGeo(string zip);
         ReverseGeo(string latitude, string longitude);
-        MakeRequest(string url);
-        GetApiKey();
-}
-
-//member functions
-Ziptastic::Ziptastic(string key)
-{
-    apiKey = key;
-}
-
-void Ziptastic::ForwardGeo()
-{
-    
-}
-
-void Ziptastic::ReverseGeo()
-{
-
-}
-
-string Ziptastic::MakeRequest()
-{
-
-}
-
-void Ziptastic::GetApiKey()
-{
-    cout << "API KEY : " << apiKey;
-}
-
-int main()
-{
-    Ziptastic Ziptastic("asdasdasdasdasd");
-    Ziptastic.GetApiKey();
 }
