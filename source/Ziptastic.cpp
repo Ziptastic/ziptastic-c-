d41a566f2b0e480382fe8934394cfbfdaffa89b9
bfranco94 <bfranco@murraystate.edu>
2016-07-13 13:46:56 -0400

    Revert "Working Object Shell"
    This reverts commit d4d95e34c6846837ac153fd920dc2daed53f2e1e.

--- source/Ziptastic.cpp
+++ source/Ziptastic.cpp
@@ -10,11 +10,11 @@ class Ziptastic
         string apiKey;
     public:
         Ziptastic(string key); // Constructor
-        void forwardGeo(string zip);
-        void reverseGeo(string latitude, string longitude);
-        void makeRequest(string url);
-        void getApiKey();
-};
+        ForwardGeo(string zip);
+        ReverseGeo(string latitude, string longitude);
+        MakeRequest(string url);
+        GetApiKey();
+}
 
 //member functions
 Ziptastic::Ziptastic(string key)
@@ -22,28 +22,28 @@ Ziptastic::Ziptastic(string key)
     apiKey = key;
 }
 
-void Ziptastic::forwardGeo(string zip)
+void Ziptastic::ForwardGeo()
 {
     
 }
 
-void Ziptastic::reverseGeo(string latitude, string longitude)
+void Ziptastic::ReverseGeo()
 {
 
 }
 
-void Ziptastic::makeRequest(string url)
+string Ziptastic::MakeRequest()
 {
 
 }
 
-void Ziptastic::getApiKey()
+void Ziptastic::GetApiKey()
 {
     cout << "API KEY : " << apiKey;
 }
 
 int main()
 {
-    Ziptastic ziptastic("asdasdasdasdasd");
-    ziptastic.getApiKey();
+    Ziptastic Ziptastic("asdasdasdasdasd");
+    Ziptastic.GetApiKey();
 }
