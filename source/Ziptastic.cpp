fc07c4ec4525f731778081ea2b451bce5f4104bc
bfranco94 <bfranco@murraystate.edu>
2016-07-13 13:22:30 -0400

    Created Ziptastic.cpp file, and basic object shell

--- source/Ziptastic.cpp
+++ source/Ziptastic.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -11,4 +12,38 @@ class Ziptastic
         Ziptastic(string key); // Constructor
         ForwardGeo(string zip);
         ReverseGeo(string latitude, string longitude);
+        MakeRequest(string url);
+        GetApiKey();
+}
+
+//member functions
+Ziptastic::Ziptastic(string key)
+{
+    apiKey = key;
+}
+
+void Ziptastic::ForwardGeo()
+{
+    
+}
+
+void Ziptastic::ReverseGeo()
+{
+
+}
+
+string Ziptastic::MakeRequest()
+{
+
+}
+
+void Ziptastic::GetApiKey()
+{
+    cout << "API KEY : " << apiKey;
+}
+
+int main()
+{
+    Ziptastic Ziptastic("asdasdasdasdasd");
+    Ziptastic.GetApiKey();
 }
